ac2b3dd28e250d58a1741111ec3b31edf87e4cd5
Luke Sedillo <lukeboarder@gmail.com>
2019-07-23 11:15:15 -0700

    Create form that posts data using body parser.

--- server.js
+++ server.js
@@ -3,11 +3,22 @@ const mustacheExpress = require('mustache-express');
 
 const app = express();
 const mustache = mustacheExpress();
+const bodyParser = require('body-parser');
 mustache.cache = null;
 app.engine('mustache',mustache);
 app.set('view engine', 'mustache');
 
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.get('/add',(req,res)=>{
+    res.render('med-form');
+});
+
+app.post('/meds/add',(req,res)=>{
+    console.log('post body',req.body);
+    res.redirect('/meds');
+});
 
 app.get('/meds',(req,res)=>{
     res.render('meds');
